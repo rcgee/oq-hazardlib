a28a34f3f622c45d5ebfa136eabde2d6912765f5
Robin Gee <geero754@gmail.com>
2015-06-30 18:26:35 +0200

    commented out ValueError about seismogenic depth being non-negative

--- openquake/hazardlib/source/point.py
+++ openquake/hazardlib/source/point.py
@@ -75,8 +75,8 @@ class PointSource(ParametricSeismicSource):
             magnitude_scaling_relationship, rupture_aspect_ratio,
             temporal_occurrence_model)
 
-        if upper_seismogenic_depth < 0:
-            raise ValueError('upper seismogenic depth must be non-negative')
+        #if upper_seismogenic_depth < 0:
+            #raise ValueError('upper seismogenic depth must be non-negative')
 
         if not lower_seismogenic_depth > upper_seismogenic_depth:
             raise ValueError('lower seismogenic depth must be below '
